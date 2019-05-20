c0550719037d731b45bae4aa65c5fb9525c63575
Steven Conaway <steven.conaway@icloud.com>
2019-05-19 20:41:45 -0700

    save lots of cpu

--- py_primes/prime.py
+++ py_primes/prime.py
@@ -9,7 +9,7 @@ def is_prime(num):
         return True
 
 
-def do_prime(n, ty):
+def do_prime(n):
     primes = []
     dists = []
     for i in range(1, n, 2):
@@ -23,31 +23,20 @@ def do_prime(n, ty):
     print(len(dists))
     print(dists)
     for i in range(1, len(primes) - 1):
-        if ty == 1:
-            x = i # primes[i]
-        else:
-            x = primes[i]
-        y = dists[i]
-        plt.scatter(x, y, s=2)
-    name = str(n) + "_x="
-    if ty == 1:
-        name = name + "number"
-    else:
-        name = name + "value"
-    print(name)
-    plt.savefig(name + ".svg")
-    plt.show()
+        plt.scatter(i, dists[i], s=2)
+    plt.savefig(str(n) + "_x=number" + ".svg")
+    plt.clf()
+
+    for i in range(1, len(primes) - 1):
+        plt.scatter(primes[i], dists[i], s=2)
+    plt.savefig(str(n) + "_x=value" + ".svg")
+    plt.clf()
+    # plt.show()
 
 
-do_prime(100, 0)
-do_prime(100, 1)
-do_prime(1000, 0)
-do_prime(1000, 1)
-do_prime(10000, 0)
-do_prime(10000, 1)
-do_prime(100000, 0)
-do_prime(100000, 1)
-do_prime(1000000, 0)
-do_prime(1000000, 1)
-do_prime(10000000, 0)
-do_prime(10000000, 1)
+do_prime(100)
+do_prime(1000)
+do_prime(10000)
+# do_prime(100000)
+# do_prime(1000000)
+# do_prime(10000000)
